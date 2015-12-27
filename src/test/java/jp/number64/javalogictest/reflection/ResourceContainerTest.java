121f05ac6aa2aa56d1e6923b78473e04754e9419
TODOROKI Daisuke <number64tech@gmail.com>
2015-12-27 11:13:32 +0900

    added testsuite.
    added testsuite.

--- src/test/java/jp/number64/javalogictest/reflection/ResourceContainerTest.java
+++ src/test/java/jp/number64/javalogictest/reflection/ResourceContainerTest.java
@@ -1,14 +1,44 @@
 package jp.number64.javalogictest.reflection;
 
+import jp.number64.javalogictest.reflection.ResourceContainerTest.Pattern01BasicTest;
+import jp.number64.javalogictest.reflection.ResourceContainerTest.Pattern02ImitationResourceTest;
+
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@RunWith(Suite.class)
+@SuiteClasses({
+    Pattern01BasicTest.class,
+    Pattern02ImitationResourceTest.class
+})
 public class ResourceContainerTest {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ResourceContainerTest.class);
 
-    @Test
-    public void case01() {
-        LOGGER.debug("ResourceContainerTest");
+    @FixMethodOrder(MethodSorters.NAME_ASCENDING)
+    public static final class Pattern01BasicTest {
+        private static final Logger LOGGER = LoggerFactory.getLogger(Pattern01BasicTest.class);
+
+        @Test
+        public void case01() {
+            LOGGER.debug("BasicTest");
+        }
     }
+
+    @FixMethodOrder(MethodSorters.NAME_ASCENDING)
+    public static final class Pattern02ImitationResourceTest {
+        private static final Logger LOGGER = LoggerFactory.getLogger(Pattern02ImitationResourceTest.class);
+
+        @Test
+        public void case01() {
+            LOGGER.debug("ImitationResourceTest");
+        }
+
+    }
+
+
 }
